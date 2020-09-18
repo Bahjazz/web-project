991fdf37782e7250739bb9e3e35ee653bd93f6bb
Bahj.ahmed111@gmail.com <bahja.ahmed111@outlook.com>
2020-09-18 12:34:48 +0200

    updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const dummyData = require('./dummy-data')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const path = require('path'); 
 
 const app = express()
 
@@ -9,19 +10,29 @@ app.engine("hbs", expressHandlebars({
 }))
 
 app.get('/', function(request, response){
-  const model = {
+   const model = {
     humans: dummyData.humans
   }
-  response.render("show-all-humans.hbs", model)
+  response.render("show-all-humans.hbs", model) 
 })
 
 app.listen(8080)
 
+app.get('/about', function(request, response){
+  response.render("about.hbs")
+})
 
-app.get('/', function(request, response){
+app.get('/index', function(request, response){
   response.render("index.hbs")
 })
 
-app.get('/about', function(request, response){
-  response.render("../about.hbs")
+app.get('/contact', function(request, response){
+  response.render("contact.hbs")
 })
+
+
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use('/static', express.static(path.join(__dirname, 'views')))
+
